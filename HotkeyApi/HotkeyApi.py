470ab1401d4fa861f87b0fa4432fb089bc5a5c0e
jfechete <60266095+jfechete@users.noreply.github.com>
2021-06-01 15:24:19 -0700

    Added 3 new endpoints, getting all programs, getting all categories, and getting all hotkeys for a program/category

--- HotkeyApi/HotkeyApi.py
+++ HotkeyApi/HotkeyApi.py
@@ -1,18 +1,44 @@
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import Hotkeys
+
+PROGRAM_ARGUMENT = "program"
+CATEGORY_ARGUMENT = "category"
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-	return Hotkeys.random_tip()
+	return jsonify(Hotkeys.get_random_hotkey())
 
-@app.route('/params/test', methods=['GET'])
+@app.route("/categories")
+def program_categories():
+	args = request.args.to_dict()
+	if PROGRAM_ARGUMENT not in args:
+		return Response("Failed to provide a \"{}\" argument".format(PROGRAM_ARGUMENT),400)
+	try:
+		return jsonify(Hotkeys.get_categories(args[PROGRAM_ARGUMENT]))
+	except ValueError as e:
+		return (str(e),400)
+
+@app.route("/programs")
+def all_programs():
+	return jsonify(Hotkeys.get_all_programs())
+
+@app.route("/hotkeys")
+def all_hotkeys():
+	args = request.args.to_dict()
+	if PROGRAM_ARGUMENT not in args:
+		return Response("Failed to provide a \"{}\" argument".format(PROGRAM_ARGUMENT),400)
+	if CATEGORY_ARGUMENT not in args:
+		return Response("Failed to provide a \"{}\" argument".format(CATEGORY_ARGUMENT),400)
+	return jsonify(Hotkeys.get_all_hotkeys_for_category(args[PROGRAM_ARGUMENT],args[CATEGORY_ARGUMENT]))
+
+@app.route("/params/test", methods=["GET"])
 def add():
-  args = request.args.to_dict()
-  return jsonify({"value": float(args["num1"])+float(args["num2"])})
+	args = request.args.to_dict()
+	return jsonify({"value": float(args["num1"])+float(args["num2"])})
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=8080)
+	app.run(host='0.0.0.0',port=8080)
--- HotkeyApi/Hotkeys.py
+++ HotkeyApi/Hotkeys.py
@@ -3,22 +3,56 @@ from random import choice
 
 DATA_FILE = "ShortcutData.csv"
 
-tips = []
+fields = [] # should be ordered as program, category, hotkey, action description
+hotkeys = []
+categories = {}
 
 def init():
-    get_tips()
+    retrieve_hotkeys()
+    retrieve_categories()
+    print(categories)
 
-def random_tip():
-    return choice(tips)
+def get_random_hotkey():
+    return choice(hotkeys)
 
-def get_tips():
-  with open(DATA_FILE,"r",encoding="utf-8") as data_file:
-    data_reader = csv.reader(data_file)
-    fields = next(data_reader)
-    for row in data_reader:
-      tip = {}
-      for i,value in enumerate(row):
-        tip[fields[i]] = value
-      tips.append(tip)
+def get_all_programs():
+    return list(categories.keys())
+
+def get_categories(program):
+    if program not in categories:
+        raise ValueError("Unknown program")
+    return categories[program]
+
+def get_all_hotkeys_for_category(program, category):
+    if program not in categories:
+        raise ValueError("Unknown program")
+    if category not in categories[program]:
+        raise ValueError("Unknown category")
+
+    hotkeys_returning = []
+    for hotkey in hotkeys:
+        if hotkey[fields[0]] == program and hotkey[fields[1]] == category:
+            hotkeys_returning.append(hotkey)
+    return hotkeys_returning
+
+def retrieve_hotkeys():
+    global fields
+    with open(DATA_FILE,"r",encoding="utf-8") as data_file:
+        data_reader = csv.reader(data_file)
+        fields = next(data_reader)
+        for row in data_reader:
+            hotkey = {}
+            for i,value in enumerate(row):
+                hotkey[fields[i]] = value
+            hotkeys.append(hotkey)
+
+def retrieve_categories():
+    program_key = fields[0]
+    category_key = fields[1]
+    for hotkey in hotkeys:
+        if hotkey[program_key] not in categories:
+            categories[hotkey[program_key]] = []
+        if hotkey[category_key] not in categories[hotkey[program_key]]:
+            categories[hotkey[program_key]].append(hotkey[category_key])
 
 init()
