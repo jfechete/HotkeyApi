4ab3da98067f448c4225d2b996b99d16d7616605
jfechete <60266095+jfechete@users.noreply.github.com>
2021-06-01 00:41:16 -0700

    Copied code from testing IDE, still need to clean up and add to code.

--- HotkeyApi/HotkeyApi.py
+++ HotkeyApi/HotkeyApi.py
@@ -1,1 +1,32 @@
 
+from flask import Flask, request, jsonify
+import csv
+from random import choice
+
+app = Flask(__name__)
+
+tips = []
+
+def get_tips():
+  with open("ShortcutData.csv","r",encoding="utf-8") as data_file:
+    data_reader = csv.reader(data_file)
+    fields = next(data_reader)
+    for row in data_reader:
+      tip = {}
+      for i,value in enumerate(row):
+        tip[fields[i]] = value
+      tips.append(tip)
+
+
+@app.route('/')
+def home():
+	return jsonify(choice(tips))
+
+@app.route('/params/test', methods=['GET'])
+def add():
+  args = request.args.to_dict()
+  return jsonify({"value": float(args["num1"])+float(args["num2"])})
+
+if __name__ == "__main__":
+  get_tips()
+  app.run(host='0.0.0.0',port=8080)
